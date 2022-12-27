2f15d5a6d12fdf6048a1a189979e57b505ef10f0
Laiba <115410922+laiba-raees3020@users.noreply.github.com>
2022-12-27 20:15:24 +0500

    Add files via upload

---
+++ src/Pages/LG-HeadForms/Program.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  Grid,
+  Typography,
+  TextField,
+  FormControl,
+  Select,
+  MenuItem
+
+} from '@mui/material';
+
+/* 
+------ MUST READ ------
+Keep the files structure similar.
+Each group must have similar folder with their
+group leader name's first letter and G followed by a - and Folder Name
+e.g: IG-HeadForms for Idrees Group
+UG = Usman Group
+RDepartment = Register Department
+*/
+
+const Program = () => {
+  const [title,subtitle] = useState();
+  
+  return (
+    <Grid container flexWrap='nowrap'>
+      <Grid item flexGrow={1}>
+        <Grid container direction='column' width='100%'>
+          <Grid item width='100%'>
+            <Typography  position= "center" variant='h3'>Program Registeration</Typography>
+          </Grid>
+          <Grid item width='100%'>
+            <Grid container width='100%'>
+              
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+                <label>Dept Id:</label>
+                <FormControl fullWidth>
+                <Select>
+                    <MenuItem value={10}>01</MenuItem>
+                    <MenuItem value={20}>02</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Start Date:</label>
+              <FormControl fullWidth>
+                <TextField
+                  variant='outlined'
+                  type='yy/mm/dd'
+                  placeholder='2022/02/09'
+                  value={subtitle}
+                />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Program Id:</label>
+                <FormControl fullWidth>
+                <Select>
+                    <MenuItem value={10}>01</MenuItem>
+                    <MenuItem value={20}>02</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Duration:</label>
+              <FormControl fullWidth>
+                <Select>
+                    <MenuItem value={10}>2 years</MenuItem>
+                    <MenuItem value={20}>4 years</MenuItem>
+                    <MenuItem value={30}>5 years</MenuItem>
+                    value={title}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Program Title:</label>
+              <FormControl fullWidth>
+              <Select>
+                    <MenuItem value={10}>BS Computer Science</MenuItem>
+                    <MenuItem value={20}>BS Architecture</MenuItem>
+                    <MenuItem value={30}>Bio-medical</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Program Type:</label>
+              <FormControl fullWidth>
+              <Select>
+                    <MenuItem value={10}>BS</MenuItem>
+                    <MenuItem value={20}>MS</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Button variant='contained' sx={{ margin: '.5em .5em .5em 0' }}  display= 'flex'
+  justify-content= 'flex-end'>
+              Save
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+   
+    </Grid>
+  );
+};
+
+export default Program;
---
+++ src/Pages/LG-HeadForms/Session.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  Grid,
+  Typography,
+  TextField,
+  FormControl,
+  Select,
+  MenuItem
+
+} from '@mui/material';
+
+/* 
+------ MUST READ ------
+Keep the files structure similar.
+Each group must have similar folder with their
+group leader name's first letter and G followed by a - and Folder Name
+e.g: IG-HeadForms for Idrees Group
+UG = Usman Group
+RDepartment = Register Department
+*/
+
+const Session = () => {
+  const [title,subtitle] = useState();
+  
+  return (
+    <Grid container flexWrap='nowrap'>
+      <Grid item flexGrow={1}>
+        <Grid container direction='column' width='100%'>
+          <Grid item width='100%'>
+            <Typography  position= "center" variant='h3'>Program Registeration</Typography>
+          </Grid>
+          <Grid item width='100%'>
+            <Grid container width='100%'>
+              
+            <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Program Type:</label>
+              <FormControl fullWidth>
+              <Select>
+                    <MenuItem value={10}>BS</MenuItem>
+                    <MenuItem value={20}>MS</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+                <label>Department</label>
+                <FormControl fullWidth>
+                <Select>
+                    <MenuItem value={10}>01</MenuItem>
+                    <MenuItem value={20}>02</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Program Id:</label>
+                <FormControl fullWidth>
+                <Select>
+                    <MenuItem value={10}>01</MenuItem>
+                    <MenuItem value={20}>02</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Session Id:</label>
+              <FormControl fullWidth>
+                <Select>
+                    <MenuItem value={10}>2k19</MenuItem>
+                    <MenuItem value={20}>2k20</MenuItem>
+                    <MenuItem value={30}>2k21</MenuItem>
+                    <MenuItem value={30}>2k22</MenuItem>
+                    value={title}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Session Start:</label>
+              <FormControl fullWidth>
+              <TextField
+                  variant='outlined'
+                  type='yy/mm/dd'
+                  placeholder='12-10-2021'
+                  value={subtitle}
+                />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Session Title:</label>
+              <FormControl fullWidth>
+              <TextField
+                  variant='outlined'
+                  type='text'
+                  value={subtitle}
+                />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Session End:</label>
+              <FormControl fullWidth>
+              <TextField
+                  variant='outlined'
+                  type='yy/mm/dd'
+                  placeholder='12-10-2021'
+                  value={subtitle}
+                />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6} padding='.5em .5em .5em 0'>
+              <label>Session Type:</label>
+              <FormControl fullWidth>
+              <Select>
+                    <MenuItem value={10}>Spring</MenuItem>
+                    <MenuItem value={20}>Fall</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Button variant='contained' sx={{ margin: '.5em .5em .5em 0' }}  display= 'flex'
+  justify-content= 'flex-end'>
+              Save
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+   
+    </Grid>
+  );
+};
+
+export default Session;
