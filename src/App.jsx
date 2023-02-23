be9e6787a126aa2ec8d0bd2b39b7a1cf8f44537e
MUK-Dev <manian1221@gmail.com>
2023-02-23 16:04:51 +0500

    Fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,6 @@ import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-<<<<<<< HEAD
-import Login from './Pages/Auth/Login';
-import AuthLayout from './Pages/Layouts/AuthLayout';
-import Register from './Pages/Auth/Register';
-import DashboardLayout from './Pages/Layouts/DashboardLayout';
-import StudentDashboard from './Pages/StudentDashboard/StudentDashboard';
-import RDepartment from './Pages/UG-HeadForms/RDepartment';
-import RStudents from './Pages/UG-HeadForms/RStudents';
-import RSubject from './Pages/UG-HeadForms/RSubject';
-import RTeachers from './Pages/UG-HeadForms/RTeachers';
-import RProgram from './Pages/UG-HeadForms/RProgram';
-import RSession from './Pages/UG-HeadForms/RSession';
-import RSemester from './Pages/UG-HeadForms/RSemester';
-import StudentAttendance from './Pages/StudentAttendance/StudentAttendance';
-import StudentCertificate from './Pages/StudentCertificate/StudentCertificate';
-import StudentProgress from './Pages/StudentProgress/StudentProgress';
-import IDepartment from './Pages/IG-HeadForms/IDepartment';
-import IProgram from './Pages/IG-HeadForms/IProgram';
-import ISemester from './Pages/IG-HeadForms/ISemester';
-import ISession from './Pages/IG-HeadForms/ISession';
-import ISubject from './Pages/IG-HeadForms/ISubject';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import StudentDetailProgress from './Pages/StudentProgress/StudentDetailProgress';
-import StudentTranscript from './Pages/StudentProgress/StudentTranscript';
-=======
 import Login from './Pages/Auth/Login'
 import Register from './Pages/Auth/Register'
 import IDepartment from './Pages/IG-HeadForms/IDepartment'
@@ -53,7 +27,6 @@ import RSubject from './Pages/UG-HeadForms/RSubject'
 import RTeachers from './Pages/UG-HeadForms/RTeachers'
 
 import AuthProvider from './Contexts/AuthContext'
->>>>>>> 49c8f64f3c8bc5356da2e0e48b28d1b541dc9839
 
 const App = () => {
   // #dcdfca
@@ -73,61 +46,6 @@ const App = () => {
   })
 
   return (
-<<<<<<< HEAD
-    <ThemeProvider theme={theme}>
-      <LocalizationProvider dateAdapter={AdapterMoment}>
-        <BrowserRouter>
-          <CssBaseline />
-          <Routes>
-            <Route element={<AuthLayout />}>
-              <Route path='/' element={<Login />} />
-              <Route path='/register' element={<Register />} />
-            </Route>
-            <Route element={<DashboardLayout />}>
-              <Route path='/student/home' element={<StudentDashboard />} />
-              <Route
-                path='/student/attendance'
-                element={<StudentAttendance />}
-              />
-              <Route
-                path='/head/register/department'
-                element={<RDepartment />}
-              />
-              <Route path='/head/register/subject' element={<RSubject />} />
-              <Route path='/head/register/students' element={<RStudents />} />
-              <Route path='/head/register/teachers' element={<RTeachers />} />
-              <Route path='/head/register/program' element={<RProgram />} />
-              <Route path='/head/register/session' element={<RSession />} />
-              <Route path='/head/register/semester' element={<RSemester />} />
-              <Route path='/student/progress' element={<StudentProgress />} />
-              <Route
-                path='/student/detail-progress'
-                element={<StudentDetailProgress />}
-              />
-              <Route
-                path='/head/register/i-department'
-                element={<IDepartment />}
-              />
-              <Route path='/head/register/i-program' element={<IProgram />} />
-              <Route path='/head/register/i-semester' element={<ISemester />} />
-              <Route path='/head/register/i-session' element={<ISession />} />
-              <Route path='/head/register/i-subject' element={<ISubject />} />
-              <Route
-                path='/student/certificate'
-                element={<StudentCertificate />}
-              />
-              <Route
-                path='/student/transcript'
-                element={<StudentTranscript />}
-              />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </LocalizationProvider>
-    </ThemeProvider>
-  );
-};
-=======
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <AuthProvider>
@@ -182,6 +100,5 @@ const App = () => {
     </BrowserRouter>
   )
 }
->>>>>>> 49c8f64f3c8bc5356da2e0e48b28d1b541dc9839
 
 export default App
