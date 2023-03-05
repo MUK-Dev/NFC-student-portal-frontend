4086b12e4bab695cabbe0cf5ff295bda2e8099e4
Idrees Malik <idreesmalik2992@gmail.com>
2023-03-05 16:44:19 +0500

    Student page finalized to add data in database

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,9 @@ import RProgram from './Pages/HeadForms/RProgram'
 import RSection from './Pages/HeadForms/RSection'
 import RSemester from './Pages/HeadForms/RSemester'
 import RSession from './Pages/HeadForms/RSession'
+import RStudent from './Pages/HeadForms/RStudent'
 import RStudentsInSubject from './Pages/HeadForms/RStudentsInSubject'
-import RSubject from './Pages/HeadForms/RStudentsInSubject'
+import RSubject from './Pages/HeadForms/RSubject'
 import RTeachersInSubject from './Pages/HeadForms/RTeachersInSubject'
 import AuthLayout from './Pages/Layouts/AuthLayout'
 import DashboardLayout from './Pages/Layouts/DashboardLayout'
@@ -79,6 +80,7 @@ const App = () => {
                     path='/head/register/students_subject'
                     element={<RStudentsInSubject />}
                   />
+                  <Route path='/head/register/student' element={<RStudent />} />
                   <Route
                     path='/head/register/teachers_subject'
                     element={<RTeachersInSubject />}
