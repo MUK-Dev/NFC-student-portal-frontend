6e2f32f1d36583803f204c7a93006f3bab075430
Laiba <115410922+laiba-raees3020@users.noreply.github.com>
2023-03-08 11:52:47 +0500

    Update StudentCertificate.jsx

--- src/Pages/StudentCertificate/StudentCertificate.jsx
+++ src/Pages/StudentCertificate/StudentCertificate.jsx
@@ -10,7 +10,7 @@ const StudentCertificate = () => {
 
       <Stack direction="row"
   justifyContent="space-around"
-  alignItems="baseline" spacing={2}>
+  alignItems="baseline" spacing={1}>
 
       <Typography>Serial No.</Typography>
           <TextField variant='standard'/>
