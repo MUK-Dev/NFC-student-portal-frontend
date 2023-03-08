151868ac8144dcb8ed82a7aed0575bfb658df5b7
Laiba <115410922+laiba-raees3020@users.noreply.github.com>
2023-03-08 13:32:31 +0500

    Update StudentCertificate.jsx

--- src/Pages/StudentCertificate/StudentCertificate.jsx
+++ src/Pages/StudentCertificate/StudentCertificate.jsx
@@ -1,4 +1,5 @@
-import { Stack, TextField, Typography, Grid, Button, Box} from '@mui/material'
+import { Stack, TextField, Typography, Grid, Button} from '@mui/material'
+import Box from '@mui/material/Box';
 import NFCLogo from '../../Assets/Images/NFC Iet Logo.png';
 
 
