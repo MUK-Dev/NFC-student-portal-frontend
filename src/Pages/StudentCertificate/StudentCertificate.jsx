3062e19fd42c27d0ab217bd5d33f8735094b16c9
Laiba <115410922+laiba-raees3020@users.noreply.github.com>
2023-03-08 12:11:01 +0500

    Update StudentCertificate.jsx

--- src/Pages/StudentCertificate/StudentCertificate.jsx
+++ src/Pages/StudentCertificate/StudentCertificate.jsx
@@ -16,7 +16,7 @@ const StudentCertificate = () => {
       <Typography>Serial No.</Typography>
           <TextField variant='standard'/>
       <img src={NFCLogo} style={{ width: 200, height: 160 }} alt='Logo'/>
-      <Typography>Reg No.</Typography>
+      <Typography sx={{ display: 'flex', justifyContent: 'flex-end'}}>Reg No.</Typography>
           <TextField variant='standard'/>
 
 </Stack>
