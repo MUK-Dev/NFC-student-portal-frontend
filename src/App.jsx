ce2f1380a2f6e8d529c02f611419dc1692321099
Laiba <115410922+laiba-raees3020@users.noreply.github.com>
2023-05-02 20:27:45 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ const App = () => {
                     element={<StudentDetailProgress />}
                   />
                   <Route
-                    path='/student/certificate'
+                    path='/student/studentcertificate'
                     element={<StudentCertificate />}
                   />
                    <Route
